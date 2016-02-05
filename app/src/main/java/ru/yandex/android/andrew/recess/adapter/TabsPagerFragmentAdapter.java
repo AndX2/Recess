2e141880d7da7fd04465b6e8dbb0898d4c53484f
And2X <Andrew.android.yandex@gmail.com>
2016-02-05 12:31:38 +0300

    Initial commit

---
+++ app/src/main/java/ru/yandex/android/andrew/recess/adapter/TabsPagerFragmentAdapter.java
@@ -0,0 +1,48 @@
+package ru.yandex.android.andrew.recess.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import ru.yandex.android.andrew.recess.R;
+import ru.yandex.android.andrew.recess.fragment.DayFragment;
+
+/**
+ * Created by Andrew on 02.02.2016.
+ */
+public class TabsPagerFragmentAdapter extends FragmentPagerAdapter {
+    private Context context;
+    private String[] tabs;
+
+    public TabsPagerFragmentAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+        tabs = (context.getResources().getStringArray(R.array.tabs));
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return DayFragment.getInstance();
+            case 1:
+                return DayFragment.getInstance();
+            case 2:
+                return DayFragment.getInstance();
+            case 3:
+                return DayFragment.getInstance();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+}
---
+++ app/src/main/java/ru/yandex/android/andrew/recess/fragment/DayFragment.java
@@ -0,0 +1,51 @@
+package ru.yandex.android.andrew.recess.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+import ru.yandex.android.andrew.recess.R;
+
+/**
+ * Created by Andrew on 02.02.2016.
+ */
+public class DayFragment extends Fragment {
+    private static final int LAYOUT = R.layout.fragment_example;
+    private View view;
+    private TextView txtView;
+    Calendar currentTime;
+
+    public static DayFragment getInstance() {
+        Bundle args = new Bundle();
+        DayFragment fragment = new DayFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(LAYOUT, container, false);
+        txtView = (TextView)view.findViewById(R.id.txt_view);
+        currentTime = Calendar.getInstance();
+        txtView.setText("day of week = " + currentTime.get(Calendar.DAY_OF_WEEK) +
+        ", first day of week = " + currentTime.getFirstDayOfWeek());
+        return view;
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //Calendar currentTime = Calendar.getInstance();
+        //txtView.setText(Integer.toString(currentTime.get(Calendar.DAY_OF_WEEK)));
+    }
+
+
+}
